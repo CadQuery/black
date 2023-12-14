ef9d4677d8a400d01d28ce19364d1852afed3f78
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2023-12-14 20:34:07 +0100

    Fix click error

--- black.py
+++ black.py
@@ -4121,7 +4121,7 @@ def patch_click() -> None:
     try:
         from click import core
         from click import _unicodefun  # type: ignore
-    except ModuleNotFoundError:
+    except ImportError:
         return
 
     for module in (core, _unicodefun):
